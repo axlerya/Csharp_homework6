67bec18540caad5fc396e1ad9132debc97395ad0
axlerya <white_cat_02@mail.ru>
2023-08-26 14:09:09 +0300

    feat: add task 1
    add a solution to the first  task

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числа через запятую");
+int[] myArray = Array.ConvertAll<string, int>(Console.ReadLine().Split(','), elem => { return System.Convert.ToInt32(elem); });
+
+int positiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Положительных чисел введеных пользователем: " + positiveNumbers(myArray));
